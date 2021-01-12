06d8bd4cef628b894c27c8760ef1d424546d7a50
Vikas Sinha <compuvikas@gmail.com>
2021-01-12 20:22:28 +0530

    IMGF-002: Fixing the error of same title problem. Added id in query and html for uniqueness.

--- src/main/java/ImageHoster/controller/ImageController.java
+++ src/main/java/ImageHoster/controller/ImageController.java
@@ -45,9 +45,10 @@ public class ImageController {
     //Also now you need to add the tags of an image in the Model type object
     //Here a list of tags is added in the Model type object
     //this list is then sent to 'images/image.html' file and the tags are displayed
-    @RequestMapping("/images/{title}")
-    public String showImage(@PathVariable("title") String title, Model model) {
-        Image image = imageService.getImageByTitle(title);
+    @RequestMapping("/images/{id}")
+    public String showImage(@PathVariable("id") Integer id, Model model) {
+        Image image = imageService.getImage(id);
+        //Image image = imageService.getImageByTitle(title);
         model.addAttribute("image", image);
         model.addAttribute("tags", image.getTags());
         return "images/image";
